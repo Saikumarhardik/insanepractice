42f598121d6c5bc3f73476aa7a7d5e96d0202383
Saikumarhardik <121499803+Saikumarhardik@users.noreply.github.com>
2025-04-03 00:00:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 provider "azurerm" {
   features {}
-}
 
+  # Use environment variables or pass subscription_id directly
+  subscription_id =  "7ab7ed14-84ca-4c95-baca-298be24e96ef"
+  client_id       = "cf153eca-db39-47d6-8f19-5c908036bfd1"
+  client_secret   = "ebkxi5bEsxFO5W1iHrmcUM"
+  tenant_id       = "0ceead1f-d661-4623-a81a-884ae36ce8a6"
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "insane-rg"
