3213ad9870934589a4e32c99788719dbf7a03382
Saikumarhardik <121499803+Saikumarhardik@users.noreply.github.com>
2025-04-03 00:20:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id =  "7ab7ed14-84ca-4c95-baca-298be24e96ef" 
